3d86373ab1a45d4c9078ad7f4f8a21d8e2fbc1ef
Melissa Nunes <nunes.melissa.m@gmail.com>
2019-07-18 10:38:48 -0700

    average function

--- averageHex.js
+++ averageHex.js
@@ -1,5 +1,7 @@
 const numberToHex = number => Math.round(number).toString(16);
 const hexToNumber = hex => parseInt(hex, 16);
+const sumReducer = (accumulator, number) => accumulator + number;
+const average = (...numbers) => numbers.reduce(sumReducer)/numbers.length;
  
 const testNumToHex1 = numberToHex(255);
 const testNumToHex2 = numberToHex(43.4);
@@ -15,7 +17,7 @@ console.assert(testHexToNumber1 === 255, testHexToNumber1);
 console.assert(testHexToNumber2 === 43, testHexToNumber2);
 console.assert(testHexToNumber3 === 44, testHexToNumber3);
 
-
-
-
-  
+const testAverage1 = average(1, 2, 3, 4, 5);
+const testAverage2 = average(-1, 2, 3, 4, 5);
+console.assert(testAverage1 === 3, testAverage1);
+console.assert(testAverage2 === 2.6, testAverage2);
