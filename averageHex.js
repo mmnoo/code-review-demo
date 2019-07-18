bf7e334bae54a52ebacfe38d960e40c7e8241dda
Melissa Nunes <nunes.melissa.m@gmail.com>
2019-07-18 11:23:45 -0700

    average hex colour function

--- averageHex.js
+++ averageHex.js
@@ -4,7 +4,17 @@ const numberToHex = number => Math.round(number).toString(16);
 const hexToNumber = hex => parseInt(hex, 16);
 const sumReducer = (accumulator, number) => accumulator + number;
 const average = (...numbers) => numbers.reduce(sumReducer)/numbers.length;
+const averageRounded = (...numbers) => Math.round(average(...numbers));
 const splitHexColor = (color) => [color.slice(0, 2), color.slice(2, 4), color.slice(4, 6)];
+const averageHexColor = (color1, color2) => {
+    const rgb1 = splitHexColor(color1).map(color => hexToNumber(color));
+    const rgb2 = splitHexColor(color2).map(color => hexToNumber(color));
+    const redHex = numberToHex(averageRounded(rgb1[0], rgb2[0]));
+    const greenHex = numberToHex(averageRounded(rgb1[1], rgb2[1]));
+    const blueHex = numberToHex(averageRounded(rgb1[2], rgb2[2]));
+
+    return redHex + greenHex + blueHex;
+}
 
 const testNumberToHex1 = numberToHex(255);
 const testNumberToHex2 = numberToHex(43.4);
@@ -25,7 +35,21 @@ const testAverage2 = average(-1, 2, 3, 4, 5);
 console.assert(testAverage1 === 3, testAverage1);
 console.assert(testAverage2 === 2.6, testAverage2);
 
+const testAverageRounded1 = averageRounded(-1, 2, 3, 4, 5);
+const testAverageRounded2 = averageRounded(-1, 2, 3, 4, 4);
+console.assert(testAverageRounded1 === 3, testAverageRounded1);
+console.assert(testAverageRounded2 === 2, testAverageRounded2);
+
 const testSplitHexColor1 = splitHexColor('017b5f');
 console.assert(testSplitHexColor1[0] === '01', testSplitHexColor1);
 console.assert(testSplitHexColor1[1] === '7b', testSplitHexColor1);
 console.assert(testSplitHexColor1[2] === '5f', testSplitHexColor1);
+
+const testAverageHexColor1 = averageHexColor('ff2b2c', 'fe2a2b');
+const testAverageHexColor2 = averageHexColor('3300ff', '99cc33');
+const testAverageHexColor3 = averageHexColor('336699', 'ff00ff');
+
+console.assert(testAverageHexColor1 === 'ff2b2c', testAverageHexColor1);
+console.assert(testAverageHexColor2 === '666699', testAverageHexColor2);
+console.assert(testAverageHexColor3 === '9933cc', testAverageHexColor3);
+
