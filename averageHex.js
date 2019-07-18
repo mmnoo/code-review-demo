a88e02a38b65df97f5f19e1ed0b7924abd74b746
Melissa Nunes <nunes.melissa.m@gmail.com>
2019-07-18 10:46:48 -0700

    split hex colour function

--- averageHex.js
+++ averageHex.js
@@ -1,14 +1,17 @@
+// exercize ignores validation
+
 const numberToHex = number => Math.round(number).toString(16);
 const hexToNumber = hex => parseInt(hex, 16);
 const sumReducer = (accumulator, number) => accumulator + number;
 const average = (...numbers) => numbers.reduce(sumReducer)/numbers.length;
- 
-const testNumToHex1 = numberToHex(255);
-const testNumToHex2 = numberToHex(43.4);
-const testNumToHex3 = numberToHex(43.6);
-console.assert(testNumToHex1 === 'ff', testNumToHex1);
-console.assert(testNumToHex2 === '2b', testNumToHex2);
-console.assert(testNumToHex3 === '2c', testNumToHex3);
+const splitHexColor = (color) => [color.slice(0, 2), color.slice(2, 4), color.slice(4, 6)];
+
+const testNumberToHex1 = numberToHex(255);
+const testNumberToHex2 = numberToHex(43.4);
+const testNumberToHex3 = numberToHex(43.6);
+console.assert(testNumberToHex1 === 'ff', testNumberToHex1);
+console.assert(testNumberToHex2 === '2b', testNumberToHex2);
+console.assert(testNumberToHex3 === '2c', testNumberToHex3);
 
 const testHexToNumber1 = hexToNumber('ff');
 const testHexToNumber2 = hexToNumber('2b');
@@ -21,3 +24,8 @@ const testAverage1 = average(1, 2, 3, 4, 5);
 const testAverage2 = average(-1, 2, 3, 4, 5);
 console.assert(testAverage1 === 3, testAverage1);
 console.assert(testAverage2 === 2.6, testAverage2);
+
+const testSplitHexColor1 = splitHexColor('017b5f');
+console.assert(testSplitHexColor1[0] === '01', testSplitHexColor1);
+console.assert(testSplitHexColor1[1] === '7b', testSplitHexColor1);
+console.assert(testSplitHexColor1[2] === '5f', testSplitHexColor1);
